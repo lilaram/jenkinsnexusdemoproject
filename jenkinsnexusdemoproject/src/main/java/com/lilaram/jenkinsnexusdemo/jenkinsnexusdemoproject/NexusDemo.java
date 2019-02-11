9335f52d8dffbf4f05dee10fe22048eb8aba1fed
USER <USER@DESKTOP-KQBHJ6M>
2019-02-11 23:11:01 +0530

    commit 1

---
+++ jenkinsnexusdemoproject/src/main/java/com/lilaram/jenkinsnexusdemo/jenkinsnexusdemoproject/NexusDemo.java
@@ -0,0 +1,10 @@
+package com.lilaram.jenkinsnexusdemo.jenkinsnexusdemoproject;
+
+public class NexusDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hari om");
+	}
+
+}
